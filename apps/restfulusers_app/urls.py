39cd969e76e9a37aa3c93eccc39490af7e35674c
RAINFOREST\Gustavo <gustavomonardez@gmail.com>
2018-05-12 12:02:20 -0700

    added urls and views

--- apps/restfulusers_app/urls.py
+++ apps/restfulusers_app/urls.py
@@ -4,9 +4,9 @@ from . import views
 urlpatterns = [
     url(r"^users$", views.index),
     url(r"^users/new$", views.new),
-    url(r"^users/<id>/edit$", views.edit),
-    url(r"^users/<id>$", views.show),
+    url(r"^users/(?P<user>\d+)/edit$", views.edit),
+    url(r"^users/(?P<user>\d+)$", views.show),
     url(r"^users/create$", views.create),
-    url(r"^users/<id>/destroy$", views.destroy),
-    url(r"^users/<id>/update$", views.update)
+    url(r"^users/(?P<user>\d+)/destroy$", views.destroy),
+    url(r"^users/update$", views.update)
 ]
--- apps/restfulusers_app/views.py
+++ apps/restfulusers_app/views.py
@@ -1,24 +1,24 @@
 from django.shortcuts import render, HttpResponse, redirect
 
 def index(request):
-    return render(request,"/restfulusers_app/index.html")
+    return render(request,"restfulusers_app/index.html")
 
 def new(request):
-    return render(request,"/restfulusers_app/new.html")
+    return render(request,"restfulusers_app/new.html")
 
-def edit(request):
-    return render(request,"/restfulusers_app/edit.html")
+def edit(request,user):
+    return render(request,"restfulusers_app/edit.html")
 
-def show(request):
-    return render(request,"/restfulusers_app/show.html")
+def show(request,user):
+    return render(request,"restfulusers_app/show.html")
 
 def create(request):
-    return redirect("users/<id>")
+    return redirect("/users/1")
 
-def destroy(request):
-    return redirect("users/")
+def destroy(request,user):
+    return redirect("/users")
 
 def update(request):
-    return redirect("users/<id>")
+    return redirect("/users/4")
 
 
